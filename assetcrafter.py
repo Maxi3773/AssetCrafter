f105adb862e0e6effbda30e7f06a66f94b15466f
Maxi <maxi1311@icloud.com>
2024-10-31 20:19:41 +0100

    Implement map creation

--- assetcrafter.py
+++ assetcrafter.py
@@ -15,6 +15,14 @@ class Asset:
         else:
             self.image = source
 
+    @property
+    def format(self) -> str:
+        return self.image.mode
+
+    @property
+    def size(self) -> tuple[int, int]:
+        return self.image.size
+
     def store(self, name: str, format_: str) -> None:
         self.image.convert(format_).save(f"out/{name}.png")
 
@@ -25,12 +33,38 @@ class AssetMap(Asset):
         self.rows: int = rows
         self.cols: int = cols
 
+    @property
+    def tile_size(self) -> tuple[int, int]:
+        return self.image.width // self.cols, self.image.height // self.rows
+
     def select(self, row: int, col: int) -> Asset:
-        width = self.image.size[0] / self.cols
-        height = self.image.size[1] / self.rows
+        width, height = self.tile_size
         x = width * col
         y = height * row
         return Asset(self.image.crop((x, y, x + width, y + height)))
+
+
+def map_(sources: list[Asset], content: list[list[list[int]]]) -> Asset:
+    tile_sizes = set()
+    for source in sources:
+        if isinstance(source, AssetMap):
+            tile_sizes.add(source.tile_size)
+        else:
+            tile_sizes.add(source.size)
+    if len(tile_sizes) != 1:
+        raise ValueError()
+    tile_width, tile_height = tile_sizes.pop()
+    size = tile_width * len(content[0]), tile_height * len(content)
+    format_ = "RGBA" if "RGBA" in [source.format for source in sources] else "RGB"
+    image = Image.new(format_, size)
+    for row, cols in enumerate(content):
+        for col, tile in enumerate(cols):
+            source = sources[tile[0]]
+            if isinstance(source, AssetMap):
+                image.paste(source.select(tile[1], tile[2]).image, (col * tile_width, row * tile_height))
+            else:
+                image.paste(source.image, (col * tile_width, row * tile_height))
+    return Asset(image)
 
 
 def main() -> None:
@@ -56,6 +90,7 @@ def main() -> None:
 
     assets: dict[str, Asset] = {}
 
+    # 1. Load sources
     for source in build_config["sources"]:
         try:
             if "rows" in source and "cols" in source:
@@ -65,6 +100,12 @@ def main() -> None:
         except FileNotFoundError:
             print(f'Missing source file "{source["path"]}"')
 
+    # 2. Process
+    for process in build_config["process"]:
+        if "map" in process:
+            assets[process["name"]] = map_([assets[source] for source in process["sources"]], process["map"])
+
+    # 3. Output
     for output in build_config["output"]:
         if (source := output["source"]) not in assets:
             print(f'Source "{source}" undefined')
